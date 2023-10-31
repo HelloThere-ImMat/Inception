197491472d238633ca2a52603656b59857554231
Mathias DORR <mdorr@made-f0Br6s16.clusters.42paris.fr>
2023-10-31 15:22:10 +0100

    removed single quotes from local host on createdb

--- srcs/requirements/mariadb/conf/createDb.sh
+++ srcs/requirements/mariadb/conf/createDb.sh
@@ -10,8 +10,8 @@
 	mysql -e "CREATE DATABASE IF NOT EXISTS ${SQL_DATABASE};"
 	mysql -e "CREATE USER IF NOT EXISTS '${SQL_USER}'@'%' IDENTIFIED BY '${SQL_PASSWORD}';"
 	mysql -e "GRANT ALL PRIVILEGES ON ${SQL_DATABASE}.* TO '${SQL_USER}'@'%' IDENTIFIED BY '${SQL_PASSWORD}';"
-	mysql -e "GRANT ALL PRIVILEGES ON *.* TO 'root'@'localhost' IDENTIFIED BY '${SQL_ROOT_PASSWORD}';"
-	#mysql -e "FLUSH PRIVILEGES;"
+	mysql -e "GRANT ALL PRIVILEGES ON *.* TO 'root'@localhost IDENTIFIED BY '${SQL_ROOT_PASSWORD}';"
+	mysql -e "FLUSH PRIVILEGES;"
 
 	sleep 5
 	mysqladmin -u root -p{SQL_ROOT_PASSWORD} shutdown

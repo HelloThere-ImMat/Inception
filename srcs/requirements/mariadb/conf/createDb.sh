6d0366b4811974ebca73ba241b6efd9bbba03803
mat <mdorr@42paris.fr>
2023-10-31 10:36:20 -0400

    errlog

--- srcs/requirements/mariadb/conf/createDb.sh
+++ srcs/requirements/mariadb/conf/createDb.sh
@@ -8,9 +8,13 @@
 	service mariadb start;
 	sleep 5
 	mysql -e "CREATE DATABASE IF NOT EXISTS ${SQL_DATABASE};"
+	echo "MAT : Database created"
 	mysql -e "CREATE USER IF NOT EXISTS '${SQL_USER}'@'%' IDENTIFIED BY '${SQL_PASSWORD}';"
+	echo "MAT : First user created"
 	mysql -e "GRANT ALL PRIVILEGES ON ${SQL_DATABASE}.* TO '${SQL_USER}'@'%' IDENTIFIED BY '${SQL_PASSWORD}';"
+	echo "MAT : Privileges granted on first user"
 	mysql -e "GRANT ALL PRIVILEGES ON *.* TO 'root'@localhost IDENTIFIED BY '${SQL_ROOT_PASSWORD}';"
+	echo "MAT : Privileges granted on root user"
 	mysql -e "FLUSH PRIVILEGES;"
 
 	sleep 5

f8ba59ae3bb870ce1100f393988b1b1f108bb60b
Mathias DORR <mdorr@made-f0Br4s12.clusters.42paris.fr>
2023-11-08 14:21:06 +0100

    changed script maria

--- srcs/requirements/mariadb/conf/createDb.sh
+++ srcs/requirements/mariadb/conf/createDb.sh
@@ -1,32 +1,29 @@
 #!/bin/bash
 
-# if [ -d "/var/lib/mysql/${SQL_DATABASE}" ]
-# then
-# 	echo "--> ${SQL_DATABASE} allready exist\n"
-# else
-#	echo "Creating database..."
-#	service mariadb start;
-#	sleep 5
-#	mysql -e "GRANT ALL PRIVILEGES ON *.* TO 'root'@localhost IDENTIFIED BY '${SQL_ROOT_PASSWORD}';"
-#	echo "MAT : Privileges granted on root user"
-#	mysql -u root -p${SQL_ROOT_PASSWORD} -e "CREATE DATABASE IF NOT EXISTS ${SQL_DATABASE};"
-#	echo "MAT : Database created"
-#	mysql -e "CREATE USER IF NOT EXISTS '${SQL_USER}'@'%' IDENTIFIED BY '${SQL_PASSWORD}';"
-#	echo "MAT : First user created"
-#	mysql -e "GRANT ALL PRIVILEGES ON ${SQL_DATABASE}.* TO '${SQL_USER}'@'%' IDENTIFIED BY '${SQL_PASSWORD}';"
-#	echo "MAT : Privileges granted on first user"
-#	mysql -e "FLUSH PRIVILEGES;"
+ if [ -d "/var/lib/mysql/${SQL_DATABASE}" ]
+ then
+ 	echo "--> ${SQL_DATABASE} allready exist\n"
+ else
+	echo "Creating database..."
+	service mariadb start;
+	sleep 1
+	mysql -e "CREATE DATABASE IF NOT EXISTS ${SQL_DATABASE};"
+	echo "MAT : Database created"
+	mysql -e "CREATE USER IF NOT EXISTS '${SQL_USER}'@'%' IDENTIFIED BY '${SQL_PASSWORD}';"
+	echo "MAT : First user created"
+	mysql -e "GRANT ALL PRIVILEGES ON ${SQL_DATABASE}.* TO '${SQL_USER}'@'%' IDENTIFIED BY '${SQL_PASSWORD}';"
+	mysql -e "GRANT ALL PRIVILEGES ON *.* TO 'root'@localhost IDENTIFIED BY '${SQL_ROOT_PASSWORD}';"
 
-#	sleep 5
-#	mysqladmin -u root -p${SQL_ROOT_PASSWORD} shutdown
-# fi
-#sleep 5
-#exec mysqld
+	sleep 1
+	mysqladmin -u root -p${SQL_ROOT_PASSWORD} shutdown
+fi
+sleep 1
+exec mysqld
 
-service mysql start
+#service mysql start
 
-mysql -u root -e "CREATE DATABASE ${SQL_DATABASE};"
-mysql -u root -e "CREATE USER '${SQL_USER}'@'%' IDENTIFIED BY '${SQL_PASSWORD}';"
-mysql -u root -e "GRANT ALL PRIVILEGES ON ${SQL_DATABASE}.* TO '${SQL_USER}'@'%';"
-mysql -u root -e "FLUSH PRIVILEGES;"
-mysql -u root -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '${SQL_ROOT_PASSWORD}';"
+#mysql -u root -e "CREATE DATABASE ${SQL_DATABASE};"
+#mysql -u root -e "CREATE USER '${SQL_USER}'@'%' IDENTIFIED BY '${SQL_PASSWORD}';"
+#mysql -u root -e "GRANT ALL PRIVILEGES ON ${SQL_DATABASE}.* TO '${SQL_USER}'@'%';"
+#mysql -u root -e "FLUSH PRIVILEGES;"
+#mysql -u root -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '${SQL_ROOT_PASSWORD}';"

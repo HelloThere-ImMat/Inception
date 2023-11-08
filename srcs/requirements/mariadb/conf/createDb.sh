ec53a18e7c9f4a539df264054798e8cac2afa950
mat <mdorr@42paris.fr>
2023-11-08 04:57:51 -0500

    Modified create db.sh to grant privileges to root before doing anything

--- srcs/requirements/mariadb/conf/createDb.sh
+++ srcs/requirements/mariadb/conf/createDb.sh
@@ -7,18 +7,18 @@
 	echo "Creating database..."
 	service mariadb start;
 	sleep 5
-	mysql -e "CREATE DATABASE IF NOT EXISTS ${SQL_DATABASE};"
+	mysql -e "GRANT ALL PRIVILEGES ON *.* TO 'root'@localhost IDENTIFIED BY '${SQL_ROOT_PASSWORD}';"
+	echo "MAT : Privileges granted on root user"
+	mysql -u root -p${SQL_ROOT_PASSWORD} -e "CREATE DATABASE IF NOT EXISTS ${SQL_DATABASE};"
 	echo "MAT : Database created"
 	mysql -e "CREATE USER IF NOT EXISTS '${SQL_USER}'@'%' IDENTIFIED BY '${SQL_PASSWORD}';"
 	echo "MAT : First user created"
 	mysql -e "GRANT ALL PRIVILEGES ON ${SQL_DATABASE}.* TO '${SQL_USER}'@'%' IDENTIFIED BY '${SQL_PASSWORD}';"
 	echo "MAT : Privileges granted on first user"
-	mysql -e "GRANT ALL PRIVILEGES ON *.* TO 'root'@localhost IDENTIFIED BY '${SQL_ROOT_PASSWORD}';"
-	echo "MAT : Privileges granted on root user"
 	mysql -e "FLUSH PRIVILEGES;"
 
 	sleep 5
-	mysqladmin -u root -p{SQL_ROOT_PASSWORD} shutdown
+	mysqladmin -u root -p${SQL_ROOT_PASSWORD} shutdown
 # fi
 sleep 5
 exec mysqld

6eb2895c446022cce4faebc7c81ef1a4f2e6bbdb
Mathias DORR <mdorr@paul-f4Br6s6.clusters.42paris.fr>
2023-11-13 12:25:37 +0100

    added new user

--- srcs/requirements/wordpress/conf/config_wp.sh
+++ srcs/requirements/wordpress/conf/config_wp.sh
@@ -22,10 +22,10 @@ else
                     --admin_user=$WP_ADMIN_USER\
                     --admin_password=$WP_ADMIN_PASSWORD\
                     --admin_email=$WP_ADMIN_EMAIL
-   #wp user create $WP_SUSCRIBER_USER $WP_SUSCRIBER_EMAIL\
-    #                --role=author\
-     #               --user_pass=$WP_SUBSCRIBER_PASSWORD\
-      #              --allow-root
+   wp user create $WP_USER $WP_USER_EMAIL\
+                    --role=author\
+                    --user_pass=$WP_USER_PASSWORD\
+                    --allow-root
 fi
 
 echo "ALL DONE"
